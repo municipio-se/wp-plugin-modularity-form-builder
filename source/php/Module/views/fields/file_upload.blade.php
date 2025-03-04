6c820c1d5a017b5366f7f296d9f4fde1f11d2107
Fredrik Johansson <jeanfredrik@gmail.com>
2025-03-04 22:30:13 +0100

    Revert changes to file upload

--- source/php/Module/views/fields/file_upload.blade.php
+++ source/php/Module/views/fields/file_upload.blade.php
@@ -1,10 +1,6 @@
 <div {!! $field['conditional_hidden'] !!} class="o-grid mod-form-field">
     <div class="o-grid-12@md">
         <div class="form-group">
-            <div hidden id="{{sanitize_title($field['name']) . '-preview'}}" class="c-imageinput__preview">
-                <div class="c-imageinput__image material-icons {{$aspectRatioClass}}"></div>
-                <span></span>
-            </div>
             @fileinput([
                 'name' => sanitize_title($field['name']),
                 'description' => !empty($field['description'])
@@ -18,9 +14,6 @@
                 'accept' => $field['filetypes'] && is_array($field['filetypes']) ? $field['attributeList']['accept'] : '',
                 'filesMax' => $field['type'] === 'multiple' ? $field['files_max'] : 1,
                 'classList' => [$field['required'] ? 'data-js-required' : ''],
-                'attributeList' => [
-                    'data-image-preview' => sanitize_title($field['name']) . '-preview',
-                ]
             ])
             @endfileinput
 
